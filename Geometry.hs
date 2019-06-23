8e3f6bde685a427ae56ad70fedae55a598ebd620
Qi Zhang <singularitti@outlook.com>
2019-06-23 04:59:54 -0400

    Add some data types and `isin` function

--- Geometry.hs
+++ Geometry.hs
@@ -3,3 +3,8 @@ module Geometry where
 data Point2D = Point2D Float Float deriving (Show)
 
 data Point3D = Point3D Float Float Float deriving (Show)
+
+data Rectangle = Rectangle { lx :: Float, rx :: Float, ly :: Float, uy :: Float } deriving (Show)
+
+isin :: Point2D -> Rectangle -> Bool
+isin (Point2D x y) (Rectangle lx rx ly uy) = lx <= x && x <= rx && ly <= y && y <= uy
